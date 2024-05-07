f93c1dfb8546b38d068ee8a137efd65ba46bc916
PuR3Luck <aidan.ong822@gmail.com>
2024-05-07 12:20:23 +0800

    Finish up interest related functions

--- Web-App/Server Code/interest_submission_fns.py
+++ Web-App/Server Code/interest_submission_fns.py
@@ -1,10 +1,44 @@
 from db_utils import sql_wrapper
+import sqlite3
 
-def submit_interest():
-  raise NotImplementedError
+@sql_wrapper
+def submit_interest(cursor, user_id: int, pet_id: int) -> bool:
+  """
+    Submits an interest for a pet by a user.
 
-def edit_interest():
-  raise NotImplementedError
+    Args:
+      cursor: sqlite3 cursor object
+      user_id: int, the ID of the user submitting the interest
+      pet_id: int, the ID of the pet the user is interested in
+    
+    Returns:
+      bool, True if the interest was successfully submitted, False otherwise
+  """
+  try:
+    cursor.execute("INSERT INTO INTERESTS (user_id, pet_id) VALUES (?, ?)", (user_id, pet_id))
+    return True
+  except sqlite3.Error as e:
+    print(f"Error occurred while submitting interest: {e}")
+    return False
 
-def delete_interest():
-  raise NotImplementedError
+@sql_wrapper
+def delete_interest(cursor, user_id: int, pet_id: int) -> bool:
+  """
+    Delete a submission of interest for a pet by a user.
+
+    Args:
+      cursor: sqlite3 cursor object
+      user_id: int, the ID of the user whose interest is to be deleted
+      pet_id: int, the ID of the pet whose interest is to be deleted
+
+    Returns:
+      bool, True if the interest was successfully deleted, False otherwise
+  """
+
+  try:
+    cursor.execute("DELETE FROM INTERESTS WHERE user_id = ? AND pet_id = ?", (user_id, pet_id))
+    return True
+  
+  except sqlite3.Error as e:
+    print(f"Error occurred while deleting interest: {e}")
+    return False
